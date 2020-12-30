9e6c0845969850eac6dcbb42ddc276ca88454592
raj911tx <35886349+raj911tx@users.noreply.github.com>
2020-12-30 20:24:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Training
+TRAIN_WIDER_PATH = "widerface/WIDER_train/"
+
+#Validation
+VAL_WIDER_PATH = "widerface/WIDER_val/"
+
+#Testing
+TEST_WIDER_PATH = "widerface/WIDER_test/"
+
+# Ground Truth
+GROUND_TRUTH_PATH = "widerface/wider_face_split/"
+
+# Output
+OUTPUT_PATH = "datasets/widerface/TF_data/"
