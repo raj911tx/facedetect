942c9179dcc1245448f73831547abe50be49df43
raj911tx <35886349+raj911tx@users.noreply.github.com>
2020-12-30 20:29:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Training
-TRAIN_WIDER_PATH = "widerface/WIDER_train/"
+TRAIN_WIDER_PATH = "data/widerface/train/images/WIDER_train/images/"
 
 #Validation
-VAL_WIDER_PATH = "widerface/WIDER_val/"
+VAL_WIDER_PATH = "data/widerface/val/images/WIDER_val/images/"
 
 #Testing
-TEST_WIDER_PATH = "widerface/WIDER_test/"
+TEST_WIDER_PATH = "data/widerface/test/images/WIDER_test/images/"
 
 # Ground Truth
-GROUND_TRUTH_PATH = "widerface/wider_face_split/"
+GROUND_TRUTH_PATH = "wider_face_split/"
 
 # Output
-OUTPUT_PATH = "datasets/widerface/TF_data/"
+OUTPUT_PATH = "data/"
